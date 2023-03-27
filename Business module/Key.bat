0a0d8efe243600abe27bca59a8624d39658b486a
CN-RPA004 (CTFCN-虚拟助手004) <CN-RPA004@CTF.SZ>
2023-03-27 15:59:14 +0800

    HRAUTHPROJECT

---
+++ Business module/Key.bat
@@ -0,0 +1,18 @@
+@echo CC-port account name and password
+
+cmdkey /generic:CTF_Credentials_WH /user:icc_rpa02 /pass:ctf@000000
+
+cmdkey /generic:CTF_Credentials_lj /user:icc_rpa02 /pass:ctf@000000
+
+cmdkey /generic:CTF_Credentials_SZ /user:icc_rpa02 /pass:ctf@000000
+
+cmdkey /generic:I Tongbao_Credential /user:admin /pass:zhoudafu@1234
+
+
+
+
+cmdkey /generic:CTF_Credentials_WH_ICS /user:SYSRPAWH01 /pass:ctfics2022
+
+cmdkey /generic:CTF_Credentials_lj_ICS /user:SYSRPALJ01 /pass:ctfics2022
+
+cmdkey /generic:CTF_Credentials_SZ_ICS /user:SYSRPASZ01 /pass:ctfics2022
---
+++ Start.bat
@@ -0,0 +1,2 @@
+cd "C:\Users\Administrator\AppData\Local\UiPath\app-21.4.4\"
+UiRobot execute --file "D:\CN-VA Project\Robot instruction path\10072292\Chow Tai Fook Mainland Authority Automation\Main.xaml"
